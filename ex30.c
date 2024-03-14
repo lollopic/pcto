9e6c85612e2cb8ce6d26754fd43801b69da08c6e
lollopic <162984852+lollopic@users.noreply.github.com>
2024-03-14 13:11:10 +0000

    yttf

--- ex30.c
+++ ex30.c
@@ -4,6 +4,34 @@ int main()
 {
 int a;
 int b;
-printf("dimmi le coordinate del punto\n")
-scanf("%d, %d", &a, &b);
+printf("dimmi le coordinate del punto e ti diró a quale quadrante appartengono\n");
+scanf("%d %d", &a, &b);
+if((a > 0) && (b > 0))
+{
+    printf("appartiene al primo quadrante\n");
+}
+else if((a < 0) && (b > 0))
+{
+    printf("appartiene al secondo quadrante\n");
+}
+else if((a < 0) && (b < 0))
+{
+    printf("appartiene al terzo quadrante\n");
+}
+else if((a > 0) && (b < 0))
+{
+    printf("appartiene al quarto quadrante\n");
+}
+else if((a == 0) && (b == 0))
+{
+    printf("il punto é l'origine/n");
+}
+else if((a == 0) && (b != 0))
+{
+    printf("il punto appartiene all'asse y\n");
+}
+else if((a != 0) && (b == 0))
+{
+    printf("il punto appartiene all'asse x\n");
+}
 }
